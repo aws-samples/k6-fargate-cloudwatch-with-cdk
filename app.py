5358f24fdf1f2f4b57b5670cd3f8930c7b6f891e
Jin, Song <sonjin@amazon.com>
2022-08-16 17:35:59 +0900

    update stack name

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from cdk.cdk_stack import CdkStack
 
 
 app = cdk.App()
-CdkStack(app, "k2-prometheus-grafana",
+CdkStack(app, "k6-fargate-cloudwatch",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
